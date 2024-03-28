49e6425759b593a6c09e2fa9a9b2555bc258261c
nick <andantej281@gmail.com>
2024-03-28 14:50:37 +0900

    init - 디렉토리 구조 세팅

--- src/main/java/com/e2i1/linkeepserver/LinkeepServerApplication.java
+++ src/main/java/com/e2i1/linkeepserver/LinkeepServerApplication.java
@@ -10,4 +10,5 @@ public class LinkeepServerApplication {
         SpringApplication.run(LinkeepServerApplication.class, args);
     }
 
+
 }
--- src/main/java/com/e2i1/linkeepserver/config/CollectionReqDTO.java
+++
@@ -1,6 +0,0 @@
-package com.e2i1.linkeepserver.config;
-
-public class CollectionReqDTO {
-    String title;
-    String description;
-}
