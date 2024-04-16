e9a35d4662e15c591bb34fa8d31eb8f4a0518a8f
oU-Ua <imuu30071126@gmail.com>
2024-04-16 23:28:17 +0900

    feat : collection 정보 가져오기

--- src/main/java/com/e2i1/linkeepserver/domain/collections/business/CollectionsBusiness.java
+++ src/main/java/com/e2i1/linkeepserver/domain/collections/business/CollectionsBusiness.java
@@ -2,6 +2,8 @@ package com.e2i1.linkeepserver.domain.collections.business;
 
 import com.e2i1.linkeepserver.common.annotation.Business;
 import com.e2i1.linkeepserver.domain.collections.converter.CollectionsConverter;
+import com.e2i1.linkeepserver.domain.collections.dto.CollectionUserResDTO;
+import com.e2i1.linkeepserver.domain.collections.entity.CollectionsEntity;
 import com.e2i1.linkeepserver.domain.collections.service.CollectionsService;
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +12,10 @@ import lombok.RequiredArgsConstructor;
 public class CollectionsBusiness {
     private final CollectionsService collectionsService;
     private final CollectionsConverter collectionsConverter;
+
+    public CollectionUserResDTO getCollection(Long id){
+        CollectionsEntity collection = collectionsService.findById(id);
+        CollectionUserResDTO result = collectionsConverter.toDTO(collection);
+        return result;
+    }
 }
--- src/main/java/com/e2i1/linkeepserver/domain/collections/converter/CollectionsConverter.java
+++ src/main/java/com/e2i1/linkeepserver/domain/collections/converter/CollectionsConverter.java
@@ -2,10 +2,23 @@ package com.e2i1.linkeepserver.domain.collections.converter;
 
 import com.e2i1.linkeepserver.common.annotation.Converter;
 import com.e2i1.linkeepserver.domain.collections.business.CollectionsBusiness;
+import com.e2i1.linkeepserver.domain.collections.dto.CollectionUserResDTO;
+import com.e2i1.linkeepserver.domain.collections.entity.CollectionsEntity;
 import lombok.RequiredArgsConstructor;
 
 @Converter
 @RequiredArgsConstructor
 public class CollectionsConverter {
+
+    public CollectionUserResDTO toDTO(CollectionsEntity collection){
+        CollectionUserResDTO userResDTO = CollectionUserResDTO.builder()
+                .title(collection.getTitle())
+                .imgUrl(collection.getImgURL())
+                .description(collection.getDescription())
+                .linkList(null)
+                .tagList(null)
+                .build();
+        return userResDTO;
+    }
 
 }
