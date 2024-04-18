47687ac4195b8e41dffba0d17c1b27094d7c605e
nick <andantej281@gmail.com>
2024-04-18 14:06:10 +0900

    fix : git ignore edit

--- src/main/java/com/e2i1/linkeepserver/domain/oauth/SocialOAuth.java
+++
@@ -1,9 +0,0 @@
-package com.e2i1.linkeepserver.domain.oauth;
-
-public interface SocialOAuth {
-    /**
-     * 각 소셜 로그인 페이지로 redirect할 URL build
-     * 사용자로부터 로그인 요청을 받아 소셜 로그인 서버 인증용 코드 요청
-     */
-    String getOAuthRedirectURL();
-}
