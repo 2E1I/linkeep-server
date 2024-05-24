2d01e4da8608c69f754f75a4ae2cfb46c1753ed2
ikhyeon <andantej281@gmail.com>
2024-05-24 22:09:05 +0900

    feat : 최근 검색 목록 개발 완료 - 10개씩 유지 - 검색어 중복 저장하지 않고, 가장 최신으로 업데이트

--- src/main/java/com/e2i1/linkeepserver/domain/users/service/RecentSearchService.java
+++ src/main/java/com/e2i1/linkeepserver/domain/users/service/RecentSearchService.java
@@ -16,14 +16,27 @@ import static com.e2i1.linkeepserver.common.error.ErrorCode.NULL_POINT;
 public class RecentSearchService {
 
     private final RedisTemplate<String, String> redisTemplate;
-    private static final String RECENT_SEARCHES_KEY_PREFIX = "user:recentSearches:";
+    private static final String RECENT_SEARCHES_KEY_PREFIX = "recent_searches:";
     private static final int MAX_RECENT_SEARCHES = 10;
     private static final String DELETE_MARKER = "__DELETE__";
 
 
     public void addSearchTerm(Long userId, String keyword) {
         String key = RECENT_SEARCHES_KEY_PREFIX + userId;
+
+        List<String> searchList = redisTemplate.opsForList().range(key, 0, -1);
+
+        if (searchList == null) {
+            throw new ApiException(NULL_POINT, "해당 유저는 최근 검색 기록이 없습니다.");
+        }
+
+        // keyword가 이미 최근 검색어 목록에 있다면 중복 저장하지 않고 가장 앞으로 가져오기 위해 삭제
+        if (searchList.contains(keyword)) {
+            redisTemplate.opsForList().remove(key, 0, keyword);
+        }
+
         redisTemplate.opsForList().leftPush(key, keyword);
+
         redisTemplate.opsForList().trim(key, 0, MAX_RECENT_SEARCHES - 1);
     }
 
