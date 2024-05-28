e67d89a12b3a148f56de396ad76a1270c51af7de
nick <andantej281@gmail.com>
2024-05-28 19:27:36 +0900

    feat : 1년 지난 탈퇴 회원 삭제 스케줄링 개발 완료 - 매일 오전 02시에 status가 UNREGISTERED이고 삭제된지 1년 넘은 유저들 DB에서 hard delete

--- src/main/java/com/e2i1/linkeepserver/domain/users/repository/UsersRepository.java
+++ src/main/java/com/e2i1/linkeepserver/domain/users/repository/UsersRepository.java
@@ -2,9 +2,14 @@ package com.e2i1.linkeepserver.domain.users.repository;
 
 import com.e2i1.linkeepserver.domain.users.entity.UserStatus;
 import com.e2i1.linkeepserver.domain.users.entity.UsersEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
 
@@ -18,4 +23,9 @@ public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
 
     List<UsersEntity> findByNicknameContainingAndStatus(String search, UserStatus status);
     UsersEntity findByNickname(String nickName);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM UsersEntity u WHERE u.status = 'UNREGISTERED' AND u.updateAt < :oneYearAgo")
+    void deleteUnregisteredUsersForOneYear(LocalDateTime oneYearAgo);
 }
--- src/main/java/com/e2i1/linkeepserver/domain/users/service/UsersService.java
+++ src/main/java/com/e2i1/linkeepserver/domain/users/service/UsersService.java
@@ -1,18 +1,21 @@
 package com.e2i1.linkeepserver.domain.users.service;
 
 
-import static com.e2i1.linkeepserver.domain.users.entity.UserStatus.REGISTERED;
-
 import com.e2i1.linkeepserver.common.error.ErrorCode;
 import com.e2i1.linkeepserver.common.exception.ApiException;
 import com.e2i1.linkeepserver.domain.users.dto.EditProfileReqDTO;
 import com.e2i1.linkeepserver.domain.users.entity.UsersEntity;
 import com.e2i1.linkeepserver.domain.users.repository.UsersRepository;
-import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
+import static com.e2i1.linkeepserver.domain.users.entity.UserStatus.REGISTERED;
 
 @Service
 @RequiredArgsConstructor
@@ -71,5 +74,11 @@ public class UsersService {
     public void deleteUser(Long userId) {
         UsersEntity user = usersRepository.findById(userId).orElseThrow();
         user.delete();
+    }
+
+    public void deleteUnregisteredUsersForOneYear() {
+        LocalDateTime cutoffDate = LocalDateTime.now().minusYears(1).with(LocalTime.MAX); // 1년 전의 하루 끝 시간으로 설정
+
+        usersRepository.deleteUnregisteredUsersForOneYear(cutoffDate);
     }
 }
