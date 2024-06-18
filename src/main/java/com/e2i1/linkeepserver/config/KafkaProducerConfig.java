c7f966ce4d0c0589f2cb5a9059a4a4e6bd5628ae
ikhyeon <andantej281@gmail.com>
2024-06-18 19:31:28 +0900

    feat : kafka 설정 중...

---
+++ src/main/java/com/e2i1/linkeepserver/config/KafkaProducerConfig.java
@@ -0,0 +1,35 @@
+package com.e2i1.linkeepserver.config;
+
+import com.e2i1.linkeepserver.domain.links.entity.LinksEntity;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${kafka.bootstrap.servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ConsumerFactory<String, LinksEntity> notificationConsumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "LINK-SAVE");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class.getName());
+        configProps.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
+        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        return new DefaultKafkaConsumerFactory<>(configProps, new StringDeserializer(), new JsonDeserializer<>(LinksEntity.class));
+    }
+}
